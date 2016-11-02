4a5f67f91b55e47539c6972b81b3f002f49dec16
mdmvsfs <mdmvsfs@gmail.com>
2016-11-02 18:52:32 -0200

    metodo para listar estados

--- src/br/com/senac/bo/EstadoBO.java
+++ src/br/com/senac/bo/EstadoBO.java
@@ -1,58 +1,64 @@
 package br.com.senac.bo;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import br.com.senac.dao.EstadoDAO;
 import br.com.senac.model.Estado;
-import java.sql.SQLException;
-
-
 
 public class EstadoBO {
-//	
-//	public void alterar(Estado e){
-//
-//		try{
-//			EstadoDAO dao=new EstadoDAO();
-//			dao.alterar(e);
-//
-//		} catch (SQLException excp){
-//			System.out.println("Erro ao alterar estado " + excp.getMessage());
-//			excp.printStackTrace();
-//		}
-//	}
-//	public void excluir(int id){
-//
-//		try{
-//			EstadoDAO dao=new EstadoDAO();
-//			dao.excluir(id);
-//
-//		} catch (SQLException excp){
-//			System.out.println("Erro ao excluir estado " + excp.getMessage());
-//			excp.printStackTrace();
-//		}
-//	}
-//	
-//	public Estado getEnderecoById(int id){
-//		
-//		Estado estado=null;
-//		
-//		try{
-//			EstadoDAO dao=new EstadoDAO();
-//			estado=dao.getEstadoById();
-//			
-//			
-//		}catch(SQLException excp){
-//			System.out.println("Erro ao listar estado. " + excp.getMessage());
-//			excp.printStackTrace();
-//		}
-//		
-//		return estado;
-//	}
+
+	public List<Estado> listaEstados() {
+		EstadoDAO dao = new EstadoDAO();
+		List<Estado> lista = null;
+		try {
+			lista = dao.listaEstados();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return lista;
+	}
+
+	//
+	// public void alterar(Estado e){
+	//
+	// try{
+	// EstadoDAO dao=new EstadoDAO();
+	// dao.alterar(e);
+	//
+	// } catch (SQLException excp){
+	// System.out.println("Erro ao alterar estado " + excp.getMessage());
+	// excp.printStackTrace();
+	// }
+	// }
+	// public void excluir(int id){
+	//
+	// try{
+	// EstadoDAO dao=new EstadoDAO();
+	// dao.excluir(id);
+	//
+	// } catch (SQLException excp){
+	// System.out.println("Erro ao excluir estado " + excp.getMessage());
+	// excp.printStackTrace();
+	// }
+	// }
+	//
+	// public Estado getEnderecoById(int id){
+	//
+	// Estado estado=null;
+	//
+	// try{
+	// EstadoDAO dao=new EstadoDAO();
+	// estado=dao.getEstadoById();
+	//
+	//
+	// }catch(SQLException excp){
+	// System.out.println("Erro ao listar estado. " + excp.getMessage());
+	// excp.printStackTrace();
+	// }
+	//
+	// return estado;
+	// }
 }
-
-
-
-
-
-
-
-
--- src/br/com/senac/dao/EstadoDAO.java
+++ src/br/com/senac/dao/EstadoDAO.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.senac.model.Estado;
 
@@ -29,5 +31,23 @@ public class EstadoDAO implements Serializable {
 		rs.close();
 
 		return e;
+	}
+
+	public List<Estado> listaEstados() throws SQLException {
+		List<Estado> lista = new ArrayList<>();
+		conn = Conexao.getConnection();
+		pstm = conn.prepareStatement("SELECT * FROM ESTADO");
+		ResultSet rs = pstm.executeQuery();
+		while (rs.next()) {
+			Estado e = new Estado();
+			e.setNome(rs.getString("nomeestado"));
+			e.setSigla(rs.getString("siglaestado"));
+			e.setId(rs.getInt("idEstado"));
+			lista.add(e);
+		}
+		rs.close();
+
+		return lista;
+
 	}
 }
